7559dfbcd66d9a19ba9f9d6b247841b1e255d125
Ruben Martirosyan <rubboss1992@gmail.com>
2023-10-19 12:59:34 +0400

    init:finished work

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 module.exports = function reverse (n) {
+    let numStr = n.toString();
+
+    let revStr = numStr.split('').reverse().join('');
   
+    let reversedNumber = parseInt(revStr, 10);
+  
+    return reversedNumber;
 }
